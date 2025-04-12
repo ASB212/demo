7523262a70f344d1212a4b840fa2bacf8760a328
Aleksandr Bogomolov <diadokrgsb@mail.com>
2025-04-12 12:25:00 +0300

    Пакеты #4. Простые имена

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,30 +1,25 @@
-package ru.courses.main;
 
-import ru.courses.city.City;
+import java.awt.Point;
+
+import ru.geometry.MyPoint;
 
 public class Main {
     public static void main(String[] args) {
-        City cityA = new City("A");
-        City cityB = new City("B");
-        City cityC = new City("C");
-        City cityD = new City("D");
-        City cityE = new City("E");
-        City cityF = new City("F");
 
-        cityB.addPath(cityC, 1);
-        cityB.addPath(cityD, 2);
-        cityD.addPath(cityA, 1);
-        cityC.addPath(cityE, 1);
-        cityE.addPath(cityF, 3);
-        cityE.addPath(cityA, 1);
+        MyPoint myPoint1 = MyPoint.A;
+        MyPoint myPoint2 = MyPoint.B;
 
-        City result1 = ru.courses.city.Travel.travelBy(cityB, 1);
-        System.out.println("Город после 1 перехода: " + (result1 != null ? result1.getName() : "Нет пути"));
 
-        City result2 = ru.courses.city.Travel.travelBy(cityB, 2);
-        System.out.println("Город после 2 переходов: " + (result2 != null ? result2.getName() : "Нет пути"));
+        Point point1 = new Point(2, 3);
+        Point point2 = new Point(4, 5);
 
-        City result3 = ru.courses.city.Travel.travelBy(cityB, 3);
-        System.out.println("Город после 3 переходов: " + (result3 != null ? result3.getName() : "Нет пути"));
+
+        System.out.println("Экземпляры класса MyPoint:");
+        System.out.println("Точка A: " + myPoint1);
+        System.out.println("Точка B: " + myPoint2);
+
+        System.out.println("Экземпляры класса Point:");
+        System.out.println("Point 1: " + point1);
+        System.out.println("Point 2: " + point2);
     }
 }
